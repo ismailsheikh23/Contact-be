b662bed24e86a69207b686930efe6010837219ae
yorkmacbook017 <ismailsheikhomar126@gmail.com>
2022-11-03 11:35:55 -0500

    init

---
+++ src/main/java/net/yorksolutions/contactbackened/ContactBackenedApplication.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.contactbackened;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactBackenedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContactBackenedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/yorksolutions/contactbackened/ContactBackenedApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.contactbackened;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactBackenedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
